06e0b0acb887e8961eaabd422c547487b0d0536a
emestabillo <emsestabillo@gmail.com>
2021-03-29 12:49:11 -0400

    Delete sidebar header files

--- src/components/SideBarHeader/SideBarHeader.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SidebarHeader from './SidebarHeader';
-
-export default {
-  title: 'SidebarHeader',
-  component: SidebarHeader,
-};
-
-export const Header = () => <SidebarHeader />;
