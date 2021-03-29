510a45f924aceb09a8f8bf735e57de2142ba7190
emestabillo <emsestabillo@gmail.com>
2021-03-29 13:04:34 -0400

    add sidebar

---
+++ src/components/SidebarHeader/SidebarHeader.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SidebarHeader from './SidebarHeader';
+
+export default {
+  title: 'SidebarHeader',
+  component: SidebarHeader,
+};
+
+export const Header = () => <SidebarHeader />;
