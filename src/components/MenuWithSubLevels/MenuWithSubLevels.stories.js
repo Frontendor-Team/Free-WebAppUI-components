6111893dd7d546a1d12abb3baba954343089d89e
emestabillo <emsestabillo@gmail.com>
2021-03-29 12:39:55 -0400

    remove left menus

--- src/components/MenuWithSubLevels/MenuWithSubLevels.stories.js
+++ src/components/MenuWithSubLevels/MenuWithSubLevels.stories.js
@@ -1,64 +1,64 @@
-import React from 'react';
-import MenuWithSubLevels from './MenuWithSubLevels';
-import { ReactComponent as Overview } from '../../assets/Overview.svg';
-import { ReactComponent as Notes } from '../../assets/Notes.svg';
-import { ReactComponent as Storage } from '../../assets/Storage.svg';
-import { ReactComponent as Messages } from '../../assets/Messages.svg';
-import { ReactComponent as Onboarding } from '../../assets/Onboarding.svg';
-import { ReactComponent as Settings } from '../../assets/Settings.svg';
+// import React from 'react';
+// import MenuWithSubLevels from './MenuWithSubLevels';
+// import { ReactComponent as Overview } from '../../assets/Overview.svg';
+// import { ReactComponent as Notes } from '../../assets/Notes.svg';
+// import { ReactComponent as Storage } from '../../assets/Storage.svg';
+// import { ReactComponent as Messages } from '../../assets/Messages.svg';
+// import { ReactComponent as Onboarding } from '../../assets/Onboarding.svg';
+// import { ReactComponent as Settings } from '../../assets/Settings.svg';
 
-export default {
-  title: 'Left style/MenuWithSubLevels',
-  component: MenuWithSubLevels,
-};
+// export default {
+//   title: 'Left style/MenuWithSubLevels',
+//   component: MenuWithSubLevels,
+// };
 
-const Template = (args) => <MenuWithSubLevels {...args} />;
+// const Template = (args) => <MenuWithSubLevels {...args} />;
 
-export const MenuLarge = Template.bind({});
-MenuLarge.args = {
-  sidebarData: [
-    {
-      text: 'Overview',
-      icon: <Overview />,
-      link: '/overview',
-      subNav: [
-        { label: 'Analytics', path: '/analytics' },
-        { label: 'Company Benefits', path: '/company-benefits' },
-        { label: 'Time Tracking', path: '/time-tracking' },
-      ],
-    },
-    {
-      text: 'Notes',
-      icon: <Notes />,
-      link: '/notes',
-    },
-    {
-      text: 'Storage',
-      icon: <Storage />,
-      link: '/storage',
-    },
-    {
-      text: 'Messages',
-      icon: <Messages />,
-      link: '/messages',
-    },
-    {
-      text: 'Onboarding',
-      icon: <Onboarding />,
-      link: '/onboarding',
-    },
-    {
-      text: 'Settings',
-      icon: <Settings />,
-      link: '/settings',
-      subNav: [
-        { label: 'General', path: '/general' },
-        { label: 'Security', path: '/security' },
-        { label: 'Privacy', path: '/privacy' },
-        { label: 'Surveys', path: '/surveys' },
-        { label: 'Languages', path: '/languages' },
-        { label: 'Subscription', path: '/subscription' },
-      ],
-    },
-  ],
-};
+// export const MenuLarge = Template.bind({});
+// MenuLarge.args = {
+//   sidebarData: [
+//     {
+//       text: 'Overview',
+//       icon: <Overview />,
+//       link: '/overview',
+//       subNav: [
+//         { label: 'Analytics', path: '/analytics' },
+//         { label: 'Company Benefits', path: '/company-benefits' },
+//         { label: 'Time Tracking', path: '/time-tracking' },
+//       ],
+//     },
+//     {
+//       text: 'Notes',
+//       icon: <Notes />,
+//       link: '/notes',
+//     },
+//     {
+//       text: 'Storage',
+//       icon: <Storage />,
+//       link: '/storage',
+//     },
+//     {
+//       text: 'Messages',
+//       icon: <Messages />,
+//       link: '/messages',
+//     },
+//     {
+//       text: 'Onboarding',
+//       icon: <Onboarding />,
+//       link: '/onboarding',
+//     },
+//     {
+//       text: 'Settings',
+//       icon: <Settings />,
+//       link: '/settings',
+//       subNav: [
+//         { label: 'General', path: '/general' },
+//         { label: 'Security', path: '/security' },
+//         { label: 'Privacy', path: '/privacy' },
+//         { label: 'Surveys', path: '/surveys' },
+//         { label: 'Languages', path: '/languages' },
+//         { label: 'Subscription', path: '/subscription' },
+//       ],
+//     },
+//   ],
+// };
